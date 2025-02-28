bcf2e1de50d2056103ff481ab3e2941a2a9fffba
Bence <51375275+BenceBarens@users.noreply.github.com>
2025-02-28 21:10:40 +0100

    Add files via upload

---
+++ js/language.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let langSwitch = document.getElementById("switch-lang");
+    let flagIcon = document.getElementById("flag-icon");
+    let currentPath = window.location.pathname;
+    let userLang = navigator.language || navigator.userLanguage;
+    let savedLang = localStorage.getItem("preferredLang");
+
+    if (!savedLang) {
+        if (userLang.startsWith("en") && !currentPath.startsWith("/en/")) {
+            localStorage.setItem("preferredLang", "en");
+            window.location.href = "/en" + currentPath;
+        } else {
+            localStorage.setItem("preferredLang", "nl");
+        }
+    }
+
+    if (currentPath.startsWith("/en/")) {
+        langSwitch.href = currentPath.replace("/en/", "/");
+        flagIcon.src = "/img/nl.svg";
+        flagIcon.alt = "Switch to Dutch";
+    } else {
+        langSwitch.href = "/en" + currentPath;
+        flagIcon.src = "/img/gb.svg";
+        flagIcon.alt = "Switch to English";
+    }
+
+    langSwitch.addEventListener("click", function () {
+        if (currentPath.startsWith("/en/")) {
+            localStorage.setItem("preferredLang", "nl");
+        } else {
+            localStorage.setItem("preferredLang", "en");
+        }
+    });
+});
