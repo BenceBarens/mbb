84ddcb72bc982f712509ceaa3daf0506fe7458f5
Bence <51375275+BenceBarens@users.noreply.github.com>
2025-05-07 21:38:00 +0200

    New footer

--- js/language.js
+++ js/language.js
@@ -45,3 +45,9 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+// Scroll to top button functionality
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
